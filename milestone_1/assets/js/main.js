7d2a0246b41a5c3dd4a5785523ec630eb2df2fb5
lucamagenes <lucamgns33@gmail.com>
2021-10-26 17:10:55 +0200

    milestone 1 aggiunto ciclo per visualizzare gli elementi

--- milestone_1/assets/js/main.js
+++ milestone_1/assets/js/main.js
@@ -5,3 +5,124 @@
 // creare un array
 // ciclare l'array
 // per ogni elemento dell'array stamparlo a schermo
+
+
+const fontIcons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+]
+
+const colors = [
+    'orange',
+    'blue',
+    'purple'
+]
+
+
+const container = document.querySelector('.cards')
+
+fontIcons.forEach((icon) => {
+
+    const elementHTML = `
+    <div class="card">
+        <i class="${icon.family} ${icon.prefix}${icon.name} fa-lg"></i>
+        <span>${icon.name.toUpperCase()}</span>
+    </div>
+    `
+
+    container.innerHTML += elementHTML;
+})
