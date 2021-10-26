d039a0b573496dfac8abc0823a21c991407f6dfb
lucamagenes <lucamgns33@gmail.com>
2021-10-26 17:33:17 +0200

    milestone 2 aggiunti colori in base al tipo di icona aggiunto stile al css

---
+++ milestone_2/assets/js/main.js
@@ -0,0 +1,138 @@
+//milestone 1
+//mostriamo in pagina tutte le icone disponibili come da layout.
+
+
+// creare un array
+// ciclare l'array
+// per ogni elemento dell'array stamparlo a schermo
+
+
+const fontIcons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+]
+
+const colors = [
+    'orange',
+    'blue',
+    'purple'
+]
+
+
+const container = document.querySelector('.cards')
+
+fontIcons.forEach((icon) => {
+
+    let colors = '';
+
+    if (icon.type == 'vegetable') {
+        color = 'orange'
+    } else if (icon.type == 'animal') {
+        color = 'blue'
+    } else if (icon.type == 'user') {
+        color = 'purple'
+    }
+
+    const elementHTML = `
+    <div class="card">
+        <i class="${icon.family} ${icon.prefix}${icon.name} fa-lg" style="color:${color} "></i>
+        <span>${icon.name.toUpperCase()}</span>
+    </div>
+    `
+
+    container.innerHTML += elementHTML;
+})
